0fcb6e0d3e1a4376eb1c177c2bd268d141eb704c
Elielson B. Gonçalves <elielsonbgoncalves@gmail.com>
2024-04-26 11:44:07 -0300

    Exercícios de interação com o usuário JS

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -2,15 +2,15 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    var hora =  data.getHours()
     
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 6 && hora <= 12) {
         //Bom dia
         img.src = 'imagens/manha.png'
         document.body.style.background = '#a9dbfd'
-    } else if (hora >= 12 && hora < 18) {
+    } else if (hora > 12 && hora < 18) {
         //Boa tarde
         img.src = 'imagens/tarde.png'
         document.body.style.background = '#f8ad6d'
